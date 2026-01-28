fd493cfa833232b6671f444f1bcbe796372f6a2f
Hazelgrover <135610834+Hazelgrover@users.noreply.github.com>
2026-01-29 00:43:24 +0700

    Add files via upload

---
+++ api_yatube/yatube_api/api/serializers.py
@@ -0,0 +1,40 @@
+from rest_framework import serializers
+
+from posts.models import Post, Group, User, Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
+    group = serializers.SlugRelatedField(slug_field='slug',
+                                         queryset=Group.objects.all(),
+                                         required=False)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
+        read_only_fields = ('author',)
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True, slug_field='username')
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
+        read_only_fields = ('author', 'post')
